86c927b975026662eebc7685228831d97caa1bf3
정효진 <jeonghyojin@keunhyojing.local>
2023-10-30 22:29:25 +0900

    2. 경주 차수 입력받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -16,9 +16,13 @@ public class Application {
     }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int race_number = 0;
         System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)로 구분)");
         String input = Console.readLine();
         List<String> cars = CarName(input);
 
+        System.out.println("시도할 회수는 몇회인가요?");
+        String input_1 = Console.readLine();
+        race_number = Integer.parseInt(input);
     }
 }
